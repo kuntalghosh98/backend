fa3c797e342a780e7cade9d2d63ee6a4e29bdac5
kuntalghosh98 <129258416+kuntalghosh98@users.noreply.github.com>
2024-11-18 15:22:51 +0530

    login

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/api/users/auth/google/callback'
+  callbackURL: 'https://backend-77kt.onrender.com/api/users/auth/google/callback'
 },
 async (token, tokenSecret, profile, done) => {
   // Find or create user in database
@@ -46,40 +46,3 @@ module.exports = passport;
 
 
 
-
-
-// const jwt = require('jsonwebtoken');
-// const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
-// const User = require('../models/User');
-
-// passport.use(new GoogleStrategy({
-//   clientID: process.env.GOOGLE_CLIENT_ID,
-//   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//   callbackURL: "/api/users/auth/google/callback"
-// },
-// async (accessToken, refreshToken, profile, done) => {
-//   const existingUser = await User.findOne({ googleId: profile.id });
-//   if (existingUser) {
-//     const token = jwt.sign({ id: existingUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-//     return done(null, { user: existingUser, token });
-//   }
-//   const newUser = new User({
-//     googleId: profile.id,
-//     name: profile.displayName,
-//     email: profile.emails[0].value
-//   });
-//   await newUser.save();
-//   const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-//   done(null, { user: newUser, token });
-// }));
-
-// passport.serializeUser((user, done) => {
-//   done(null, user.user._id);
-// });
-
-// passport.deserializeUser((id, done) => {
-//   User.findById(id, (err, user) => {
-//     done(err, user);
-//   });
-// });
