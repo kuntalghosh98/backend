63aa4e47360485bc845c8d705b1217d799c22337
kuntalghosh98 <129258416+kuntalghosh98@users.noreply.github.com>
2025-08-10 12:33:09 +0530

    user redesign

--- controllers/uploadController.js
+++ controllers/uploadController.js
@@ -21,26 +21,3 @@ exports.uploadImage = async (req, res) => {
   }
 };
 
-exports.updateProfile = async (req, res) => {
-  try {
-    const userId = req.user.id;
-    const { firstName, lastName, dob, gender, mobile } = req.body;
-
-    const updatedUser = await User.findByIdAndUpdate(
-      userId,
-      {
-        firstName,
-        lastName,
-        dob,
-        gender,
-        mobile,
-      },
-      { new: true }
-    ).select('-password');
-
-    res.status(200).json({ message: 'Profile updated successfully', user: updatedUser });
-  } catch (error) {
-    console.error('Error updating profile:', error);
-    res.status(500).json({ message: 'Server error' });
-  }
-};
