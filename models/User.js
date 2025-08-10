be68d6eab9a50c57bd10910504d52853bf0b3353
kuntalghosh98 <129258416+kuntalghosh98@users.noreply.github.com>
2025-08-10 10:44:36 +0530

    user redesign

--- models/User.js
+++ models/User.js
@@ -1,4 +1,50 @@
-// models/User.js
+// // models/User.js
+// const mongoose = require('mongoose');
+
+// const otpSchema = new mongoose.Schema({
+//   otp: { type: String },
+//   expiry: { type: Date },
+// }, { _id: false });
+
+// const userSchema = new mongoose.Schema({
+//   googleId: {
+//     type: String,
+//     unique: true,
+//     sparse: true,
+//   },
+//   name: {
+//     type: String,
+//     trim: true,
+//   },
+//   email: {
+//     type: String,
+//     unique: true,
+//     sparse: true,
+//     lowercase: true,
+//     trim: true,
+//   },
+//   emailOTP: otpSchema,
+//   mobile: {
+//     type: String,
+//     unique: true,
+//     sparse: true,
+//   },
+//   mobileOTP: otpSchema,
+//   password: {
+//     type: String,
+//     minlength: 6,
+//   },
+//   createdAt: {
+//     type: Date,
+//     default: Date.now,
+//   }
+// }, { timestamps: true });
+
+// module.exports = mongoose.model('User', userSchema);
+
+
+
+
 const mongoose = require('mongoose');
 
 const otpSchema = new mongoose.Schema({
@@ -11,6 +57,21 @@ const userSchema = new mongoose.Schema({
     type: String,
     unique: true,
     sparse: true,
+  },
+  firstName: {
+    type: String,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    trim: true,
+  },
+  dob: {
+    type: Date,
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female', 'Other'],
   },
   name: {
     type: String,
@@ -41,4 +102,3 @@ const userSchema = new mongoose.Schema({
 }, { timestamps: true });
 
 module.exports = mongoose.model('User', userSchema);
-
